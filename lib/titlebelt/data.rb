18ceea895fe48332e7a83a4897e4151833b86b80
Matt Catrett <mcatrett@turbosquid.com>
2013-11-28 22:46:51 -0600

    refactor

---
+++ lib/titlebelt/data.rb
@@ -0,0 +1,68 @@
+module TitleBelt
+  class Data
+    require 'open-uri'
+    require 'csv'
+
+    class << self
+
+      def grab
+        url ||= 'http://homepages.cae.wisc.edu/~dwilson/rsfc/history/howell/'
+
+        (1869..2011).each do |year|
+          begin
+            open("../data/#{year}.txt", 'wb') do |file|
+              puts "getting #{year}..."
+              file << open("#{url}cf#{year}gms.txt").read
+              puts "done!"
+            end
+          rescue
+            puts "could not get a file for the year #{year}"
+          end
+        end
+      end
+
+      def process(start_year = 1971)
+        results = [] # [ [day, winner, loser], * ]
+
+        epoch_date = Date.new(start_year,1,1)
+
+        (1869...2011).each do |year|
+          begin
+            open("../data/#{year}.txt") do |file|
+              file.each do |row|
+                if row.gsub(/\s/,'').length > 1
+                  row.squeeze!(' ').gsub!(/\s[@].+/,'')
+
+                  date = Date.strptime(row[0..9], '%m/%d/%Y')
+                  break if date < epoch_date
+
+                  days = (date - epoch_date).to_i
+
+                  row = row[11..-1]
+                  split_row = row.split(/(\b(?=\d))|((?<=\d)\b)/)
+
+                  tie_score = split_row[2].to_i == split_row[6].to_i
+
+                  if split_row[2].to_i > split_row[6].to_i
+                    winning_team = split_row[0].strip
+                    losing_team = split_row[4].strip
+                  else
+                    winning_team = split_row[4].strip
+                    losing_team = split_row[0].strip
+                  end
+
+                  results << [days, winning_team, losing_team, tie_score, date]
+
+                end
+              end
+            end
+          rescue Exception => e
+            puts "#{e.message}"
+          end
+        end
+        results
+      end
+    end
+  end
+end
+
---
+++ lib/titlebelt/runner.rb
@@ -0,0 +1,46 @@
+module TitleBelt
+  class Runner
+  
+    class << self
+      def analyze(team = 'Nebraska', start_year = 1971)
+        data = Data.process(start_year)
+        date, winner = nil, nil
+
+        results = [team]
+        streak = 0
+
+        data.each do |game|
+          if team.downcase == game[1].downcase || team.downcase == game[2].downcase
+            date = game[0]
+            game[1].downcase == team.downcase ? winner = game[1] : winner = game[2]
+            break
+          end
+        end
+
+
+
+        raise 'team not found' unless date
+
+        data.each do |game|
+          # next game where previous winner is loser, and this game isn't a tie
+          if game[0] > date && game[2] == winner && game[3] == false
+            winner = game[1]
+            date = game[0]
+            results[-1] = [results[-1], streak, game[4]]
+            streak = 0
+            results << winner
+          else  
+            streak += 1 if game[1] == winner
+          end
+        end
+
+        puts "Streak - Team"
+        results.each do |result|
+          puts "#{result[0]} (#{result[1]}) - #{result[2]}"
+        end
+
+        return nil
+      end
+    end
+  end
+end
